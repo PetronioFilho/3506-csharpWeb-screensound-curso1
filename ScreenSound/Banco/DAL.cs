5a9e3d89f9123d8ad22c7a5a5478e0ec30494629
PetronioFilho <camposfilho84@gmail.com>
2025-11-13 16:45:09 -0300

    Implementando tipos genéricos

---
+++ ScreenSound/Banco/DAL.cs
@@ -0,0 +1,13 @@
+﻿namespace ScreenSound.Banco
+{
+    internal abstract class DAL<T>
+    {
+        public abstract IEnumerable<T> Listar();
+
+        public abstract void Adicionar(T objeto);
+
+        public abstract void Atualizar(T objeto);
+
+        public abstract void Deletar(T objeto);
+    }
+}
